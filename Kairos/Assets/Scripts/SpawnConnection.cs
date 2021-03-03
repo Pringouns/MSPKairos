5b8f4a7e90f4ba219b1676df65f5515636bfe81d
Jan-Ole-Biewig <jan268@schule.bremen.de>
2021-03-03 13:04:01 +0100

    Generator v 1.0

---
+++ Kairos/Assets/Scripts/SpawnConnection.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnConnection : MonoBehaviour
+{
+    public GameObject[] standardObjects;
+    public GameObject[] specialObjects;
+    public int maxObjects;
+    public static int standardObjectCount = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(standardObjectCount < maxObjects)
+        {
+            int rand = Random.Range(0, standardObjects.Length);
+            Instantiate(standardObjects[rand], transform.position, Quaternion.identity);
+        }
+        else
+        {
+            int rand = Random.Range(0, specialObjects.Length);
+            Instantiate(specialObjects[rand], transform.position, Quaternion.identity);
+        }
+
+        standardObjectCount++;
+    }
+}
