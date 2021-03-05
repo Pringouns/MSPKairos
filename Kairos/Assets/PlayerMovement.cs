a64d85ae15de65042e497cd2599a43d01746122d
Mouhammad Talas <mouhammad.talas247@gmail.com>
2021-03-05 09:45:16 +0100

    added Cannon, but not complete yet

---
+++ Kairos/Assets/PlayerMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float movementSpeed;
+    public Rigidbody2D rb;
+    public float jumpForce = 20f;
+    float mx;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        mx = Input.GetAxisRaw("Horizontal");
+        if (Input.GetButtonDown("Jump")) 
+        {
+            Jump();
+        }
+        
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 movement = new Vector2(mx * movementSpeed, rb.velocity.y);
+
+        rb.velocity = movement;
+
+    }
+
+    void Jump() 
+    {
+        Vector2 movement = new Vector2(rb.velocity.x, jumpForce);
+
+        rb.velocity = movement;
+    }
+
+}
---
+++ Kairos/Assets/cannonScript.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cannonScript : MonoBehaviour
+{
+
+    public float Range;
+    //public GameObject Cannon;
+    public Transform Target;
+    bool Detected = false;
+    public GameObject Cannonball;
+    public float FireRate;
+    float nextTimeToFire = 0;
+    public float Force;
+    public Transform shootPoint;
+    Vector2 Direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 targetPos = Target.position;
+
+        Direction = targetPos - (Vector2)transform.position;
+
+        RaycastHit2D rayInfo = Physics2D.Raycast(transform.position,Direction,Range);
+
+
+        if (rayInfo) 
+        {
+            if (rayInfo.collider.gameObject.tag == "Player")
+            {
+                if (Detected == false)
+                {
+                    Detected = true;
+                }
+            }
+
+            else
+            {
+                if (Detected == true) 
+                {
+                    Detected = false;
+                }
+            }
+
+        }
+
+        if (Detected) 
+        {
+ //           Cannon.transform.up = Direction;
+            if (Time.time > nextTimeToFire) 
+            {
+                nextTimeToFire = Time.time + 1 / FireRate;
+                shoot();
+            }
+        }
+
+    }
+
+    void shoot() 
+    {
+        GameObject CannonballIns = Instantiate(Cannonball, shootPoint.position, Quaternion.identity);
+        CannonballIns.GetComponent<Rigidbody2D>().AddForce(Direction * Force);
+    }
+
+
+    void OnDrawGizmosSelected() 
+    {
+        Gizmos.DrawWireSphere(transform.position, Range);
+    }
+}
