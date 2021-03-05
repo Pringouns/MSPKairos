5df943f5c79257607402a24b967e96abcc6456a1
BennetGitHub <78901655+BennetGitHub@users.noreply.github.com>
2021-03-05 10:20:37 +0100

    commentarys added
    added commentarys

--- Kairos/Assets/Scripts/CharacterController2D.cs
+++ Kairos/Assets/Scripts/CharacterController2D.cs
@@ -3,23 +3,24 @@ using UnityEngine.Events;
 
 public class CharacterController2D : MonoBehaviour
 {
-   [SerializeField] private Vector3 spawn = new Vector3(-6, -3, 0);
-	[SerializeField] private float m_JumpForce = 400f;							// Amount of force added when the player jumps.
-   [SerializeField] private int m_LifePoints = 100;         // lp - default 100 
-	[Range(0, 1)] [SerializeField] private float m_CrouchSpeed = .36f;			// Amount of maxSpeed applied to crouching movement. 1 = 100%
+   //SerializeField is for UnityInterface - edit field for the variables
+   [SerializeField] private Vector3 spawn = new Vector3(-6, -3, 0);           // Spawn Point when LP <= 0
+	[SerializeField] private float m_JumpForce = 400f;							      // Jump strength of the player
+   [SerializeField] private int m_LifePoints = 100;                           // lp - default 100 
+	[Range(0, 1)] [SerializeField] private float m_CrouchSpeed = .36f;			// maxSpeed at Crouch movement, 1=100%
 	[Range(0, .3f)] [SerializeField] private float m_MovementSmoothing = .05f;	// How much to smooth out the movement
-	[SerializeField] private bool m_AirControl = false;							// Whether or not a player can steer while jumping;
-	[SerializeField] private LayerMask m_WhatIsGround;							// A mask determining what is ground to the character
-	[SerializeField] private Transform m_GroundCheck;							// A position marking where to check if the player is grounded.
-	[SerializeField] private Transform m_CeilingCheck;							// A position marking where to check for ceilings
-	[SerializeField] private Collider2D m_CrouchDisableCollider;				// A collider that will be disabled when crouching
+	[SerializeField] private bool m_AirControl = false;							   // for movement(left,right) while jumping
+	[SerializeField] private LayerMask m_WhatIsGround;							      // LayerMask for checking Ground(you can move script data in)
+	[SerializeField] private Transform m_GroundCheck;							      // A position marking where to check if the player is grounded.
+	[SerializeField] private Transform m_CeilingCheck;							      // A position marking where to check for ceilings
+	[SerializeField] private Collider2D m_CrouchDisableCollider;				   // A collider that will be disabled when crouching
 
-	const float k_GroundedRadius = .2f; // Radius of the overlap circle to determine if grounded
-	private bool m_Grounded;            // Whether or not the player is grounded.
-	const float k_CeilingRadius = .2f; // Radius of the overlap circle to determine if the player can stand up
-	private Rigidbody2D m_Rigidbody2D;
-	private bool m_FacingRight = true;  // For determining which way the player is currently facing.
-	private Vector3 m_Velocity = Vector3.zero;
+	const float    k_GroundedRadius = .2f;             // Radius of the overlap circle to determine if grounded
+	private bool   m_Grounded;                         // Whether or not the player is grounded.
+	const float    k_CeilingRadius = .2f;              // Radius of the overlap circle to determine if the player can stand up
+	private        Rigidbody2D m_Rigidbody2D;
+	private bool   m_FacingRight = true;               // For determining which way the player is currently facing.
+	private        Vector3 m_Velocity = Vector3.zero;
 
 	[Header("Events")]
 	[Space]
@@ -152,15 +153,15 @@ public class CharacterController2D : MonoBehaviour
    }
    private void GetHealth(int health) // Added the Health to actual LifePoints
    {
-      m_LifePoints = m_LifePoints + health;
+      m_LifePoints = m_LifePoints + health;  
    }
    public int GetLifePoints() // return the actual LifePoints of the Player
    {
-      return m_LifePoints;
+      return m_LifePoints;       // actual lifepoints
    }
    public void PlayerRespawn() // Teleport the Player to Spawn Point and Reset the LifePoints
    {
-         transform.position = spawn;
-         m_LifePoints = 100;
+         transform.position = spawn; // transform position of player to spawn
+         m_LifePoints = 100;  // set LP up to 100
    }
 }
