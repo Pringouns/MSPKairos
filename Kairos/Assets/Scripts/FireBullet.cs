1e6c7300a38364d3ac330c230df1ba8133457f64
Mouhammad Talas <mouhammad.talas247@gmail.com>
2021-06-03 18:50:35 +0200

    added the ability that the boss get damaged when the player shooting the boss

--- Kairos/Assets/Scripts/FireBullet.cs
+++ Kairos/Assets/Scripts/FireBullet.cs
@@ -4,27 +4,39 @@ using UnityEngine;
 
 public class FireBullet : MonoBehaviour
 {
-   public CharacterController2D ctrl_Player;
-   public cannonScript ctrl_cannon; // zum testen wegen dmg
-   public float speed        = 20f;
+    //--------------------
+    // this script is just for the bullet of the player fire weapon!!
+    //--------------------
+    public CharacterController2D ctrl_Player;
+    public EnemyMelee ctrl_enemy; // zum testen wegen dmg
+    public BossScript ctrl_bossEnemy;
+    public float speed = 20f;
 
-   //public GameObject Enemy;
-   public Rigidbody2D rb;
+    //public GameObject Enemy;
+    public Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-       ctrl_cannon = FindObjectOfType<cannonScript>();
-       ctrl_Player = FindObjectOfType<CharacterController2D>();
-       rb.velocity = transform.right * speed;
+        ctrl_enemy = FindObjectOfType<EnemyMelee>();
+        ctrl_Player = FindObjectOfType<CharacterController2D>();
+        ctrl_bossEnemy = FindObjectOfType<BossScript>();
+        rb.velocity = transform.right * speed;
+        Destroy(this.gameObject, 1); // destroy after 5 seconds
     }
 
     void OnTriggerEnter2D(Collider2D Col)
     {
-       if (Col.CompareTag("Enemy"))
-       {
-          ctrl_cannon.m_CannonLP -= 50;
-          //ctrl_Player.GetDamage(bulletLPremove); // 20 dmg bei fernkampfangriff
-          Destroy(this.gameObject);
-       }
+        if (Col.CompareTag("Enemy"))
+        {
+            //ctrl_Player.GetDamage(bulletLPremove); // 20 dmg bei fernkampfangriff
+
+            ctrl_enemy.TakeDamage(20);
+            Destroy(this.gameObject);
+        }
+        if (Col.CompareTag("Boss"))
+        {
+            ctrl_bossEnemy.GetDamage(20);
+            Destroy(this.gameObject);
+        }
     }
 }
